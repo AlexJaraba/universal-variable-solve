093314c606892ebbaa3296cd540598346e1442fb
Your Name <10409600+djmunoz@users.noreply.github.com.>
2025-06-20 15:21:06 -0700

    Refactoring: updated __init__.py in core/

--- src/univ_prop/core/__init__.py
+++ src/univ_prop/core/__init__.py
@@ -2,5 +2,6 @@
 
 from .stumpff import stumpff_C, stumpff_S
 from .kepler import solve_chi
-11
-__all__ = ["stumpff_C", "stumpff_S", "solve_chi"]
+from .Newton_Solve import Newton_Solver
+
+__all__ = ["stumpff_C", "stumpff_S", "solve_chi","Newton_Solver"]
