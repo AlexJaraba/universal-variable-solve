9f0849af241db2e0173ce1a0b78f8754cabea6d0
Alex Jaraba <alexjaraba4@gmail.com>
2025-06-27 09:10:34 -0700

    Refactoring: Added class UniversalPropagator to propagator.py

--- src/univ_prop/api/propagator.py
+++ src/univ_prop/api/propagator.py
@@ -4,34 +4,42 @@ from ..core.kepler import solve_chi
 from ..core.coef import f, g, df, dg
 from ..core.stumpff import stumpff_C, stumpff_S
 
-def propagate(r0_vec, v0_vec, mu, dt, tol=1e-8):
-    r0_vec = np.array(r0_vec)
-    v0_vec = np.array(v0_vec)
-    r0 = norm(r0_vec)
-    v0 = norm(v0_vec)
-    vr = np.dot(r0_vec, v0_vec) / r0
-
-    alpha = 2 / r0 - v0**2 / mu
-
-    try:
-        chi = solve_chi(mu, alpha, r0_vec, vr, dt, tol=tol, verbose=False)
-    except RuntimeError as e:
-        print("Propagation failed:", e)
-        raise
-
-    z = alpha * chi**2
-    C = stumpff_C(z)
-    S = stumpff_S(z)
-
-    _f = f(chi,r0,C)
-    _g = g(dt,chi,mu,S)
-
-    r_vec = _f * r0_vec + _g * v0_vec
-    r = norm(r_vec)
-
-    fdot = df(chi,r0,r,S,z,mu)
-    gdot = dg(chi,r,C)
-
-    v_vec = fdot * r0_vec + gdot * v0_vec
+class UniversalPropagator:
+    def __init__(self,mu):
+        self.mu = mu
+        self.tol = 1e-8
     
-    return r_vec, v_vec
+    def propagate(self,position_vector,velocity_vector,dt):
+        mu = self.mu
+        tol = self.tol
+
+        r0_vec = np.array(position_vector)
+        v0_vec = np.array(velocity_vector)
+        r0 = norm(r0_vec)
+        v0 = norm(v0_vec)
+        vr = np.dot(r0_vec, v0_vec) / r0
+
+        alpha = 2 / r0 - v0**2 / mu
+
+        try:
+            chi = solve_chi(mu, alpha, r0_vec, vr, dt, tol=tol, verbose=False)
+        except RuntimeError as e:
+            print("Propagation failed:", e)
+            raise
+
+        z = alpha * chi**2
+        C = stumpff_C(z)
+        S = stumpff_S(z)
+
+        _f = f(chi,r0,C)
+        _g = g(dt,chi,mu,S)
+
+        r_vec = _f * r0_vec + _g * v0_vec
+        r = norm(r_vec)
+
+        fdot = df(chi,r0,r,S,z,mu)
+        gdot = dg(chi,r,C)
+
+        v_vec = fdot * r0_vec + gdot * v0_vec
+        
+        return r_vec, v_vec
