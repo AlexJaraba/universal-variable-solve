baf21ff6a592a8c9220ebe99a0241fbe09d25ab4
Alex Jaraba <alexjaraba4@gmail.com>
2025-06-30 08:30:46 -0700

    Added norm function to kepler.h

--- src/univ_prop/cpp/kepler.h
+++ src/univ_prop/cpp/kepler.h
@@ -3,3 +3,4 @@
 
 double solve_chi(double mu, double alpha, const std::vector<double>& r0, double vr, double dt,
                   double tol=1e-8, int max_iter=100);
+double norm(const std::vector<double>& v);
