34732d9485eb78db65d04e074418d845042d326a
Alex Jaraba <alexjaraba4@gmail.com>
2025-07-29 09:33:36 -0700

    Edited propagator.h file for better readablitiy.

--- src/univ_prop/include/propagator.h
+++ src/univ_prop/include/propagator.h
@@ -1,22 +1,28 @@
-#ifndef UNIVERSAL_PROPAGATOR_HPP
-#define UNIVERSAL_PROPAGATOR_HPP
+#ifndef UNIV_PROP_H
+#define UNIV_PROP_H
 
+#include <iostream>
 #include <vector>
+#include <cmath>
+#include <stdexcept>
 #include <string>
-#include <utility> // for std::pair
+#include <utility>
 
-class UniversalPropagator {
-public:
-    std::string name;
-    double mu;
-    double tol;
+namespace univ_prop {
+    class UniversalPropagator {
+    public:
+        std::string name;
+        double mu;
+        double tol;
 
-    UniversalPropagator(double mu_, const std::string& name_);
-    std::pair<std::vector<double>, std::vector<double>> propagate(
-        const std::vector<double>& position,
-        const std::vector<double>& velocity,
-        double dt
-    );
-};
+        UniversalPropagator(double mu_, const std::string& name_)
+            : name(name_), mu(mu_), tol(1e-8) {}
 
-#endif // UNIVERSAL_PROPAGATOR_HPP
+        std::pair<std::vector<double>, std::vector<double>> propagate(
+            const std::vector<double>& Position, 
+            const std::vector<double>& Velocity, 
+            double time_step);
+    };
+}
+
+#endif
